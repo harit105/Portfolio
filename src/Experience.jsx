50f45481abd7913cdf0d322de42e6051ec75e361
Harit Vipulbhai Shah <60238241+harit105@users.noreply.github.com>
2024-11-24 21:29:44 -0800

    edits

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,40 @@
-import { OrbitControls } from '@react-three/drei'
+import { PresentationControls, Float , Environment, useGLTF, ContactShadows } from '@react-three/drei'
 
 export default function Experience()
 {
+    const computer = useGLTF('https://threejs-journey.com/resources/models/macbook_model.gltf')
     return <>
-
-        <OrbitControls makeDefault />
         
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+        <Environment preset = "city" />
+
+        <color attach="background" args={['#241a1a']} />
+
+        
+        <PresentationControls
+            global 
+            rotation={[0.13, 0.1, 0]} 
+            polar = {[ -0.4 , 0.2]}
+            azimuth={[- 1, 0.75]}
+            config={{ mass : 2, tension : 200}}
+            snap= {{mass : 4, tension : 300}}
+        >    
+            <Float rotationIntensity={0.4}>           
+                <primitive 
+                object = {computer.scene}
+                position-y = {-1.2}
+                />
+            </Float>
+        </PresentationControls>
+
+        <ContactShadows 
+            position-y={-1.4}
+            opacity={0.5}
+            scale={5}
+            blur={2.4}
+        />    
+
 
     </>
 }
+
+
